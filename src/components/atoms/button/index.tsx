7e95c081ac630519300a4bc19c947df6f3f9359a
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-01-12 00:52:06 +0900

    ＠emotion/coreはtypescriptで使えないのでemotionを使う。 ＠emotion/styledは大丈夫

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,22 +1,30 @@
-import { css } from "emotion"; // css モジュールをインポート
 import * as React from "react";
+import { css } from "emotion";
+import styled from "@emotion/styled";
 
 const myStyle = css({
   color: "green",
   fontSize: "3rem",
   fontWeight: "bold",
-  ":hover": { color: "red" }
+  ":hover": { color: "pink" }
 });
 
 const layout = css({
   textAlign: "center"
 });
 
+const Button = styled.button({
+  color: "green",
+  height: 50,
+  ":hover": { color: "red" }
+});
+
 class App extends React.Component {
   public render() {
     return (
-      <div  className={layout}>
+      <div className={layout}>
         <p className={myStyle}>Hello emotion 👩‍🎤</p>
+        <Button>test</Button>
       </div>
     );
   }
